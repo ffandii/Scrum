79f043dff27db93fdca27f5680ef836fb5d5c73a
ffandii <13593961606@163.com>
2016-01-03 16:32:59 +0800

    add app.js

---
+++ client/src/app/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/1/3 0003.
+ */
+
+angular.module('app').constant('MONGOLAB_CONFIG',{  //连接mongodb的配置文件
+    baseUrl: '/databases/',
+    dbName: 'ffandii'
+});
