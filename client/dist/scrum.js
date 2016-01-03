d394cc322340f7a5ecaee207bb4ed535bb35a010
ffandii <13593961606@163.com>
2016-01-03 14:12:12 +0800

    deleted test.js and test.spec.js

---
+++ client/dist/scrum.js
@@ -0,0 +1,9 @@
+/* scrum - v 0.0.1 - 2015-12-19 
+https://github.com/ffandii/Scrum 
+ * Copyright (c) 2015 ffandii 
+*/
+ angular.module('templates.app', []);
+
+
+angular.module('templates.common', []);
+
