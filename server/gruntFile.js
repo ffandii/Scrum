4f678bf617b4f8ec11a7399b633ac863dd02f8e3
ffandii <13593961606@163.com>
2016-01-05 15:28:07 +0800

    add package.json and gruntFile.js for server folder

---
+++ server/gruntFile.js
@@ -0,0 +1,43 @@
+/* Global modules */
+
+module.exports = function( grunt ) {
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    //project configuration
+    grunt.initConfig({
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'default timestamp'
+        },
+        jshint: {
+            files: ['gruntFile.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                globals: {require: false, __dirname: false, console: false,module: false, exports: false}
+            }
+        }
+    });
+
+    //Default tasks
+    grunt.registerTask('default',['jshint']);
+
+    grunt.registerTask('timestamp',function(){
+        grunt.log.subhead(Date());
+    });
+
+    grunt.registerTask('supervise',function(){
+        this.async();
+        require('supervisor').run(['server.js']);
+    });
+
+};
