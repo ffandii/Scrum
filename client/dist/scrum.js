8fc699f90bfead032b514f3ccdbefe58581f040e
ffandii <13593961606@163.com>
2016-01-08 20:11:19 +0800

    files created by grunt

---
+++ client/dist/scrum.js
@@ -0,0 +1,17 @@
+/* scrum - v 0.0.1 - 2016-01-08 
+https://github.com/ffandii/Scrum 
+ * Copyright (c) 2016 ffandii 
+*/
+ /**
+ * Created by Administrator on 2016/1/3 0003.
+ */
+
+angular.module('app',[]).constant('MONGOLAB_CONFIG',{  //connect ongodb config
+    baseUrl: '/databases/',
+    dbName: 'ffandii'
+});
+angular.module('templates.app', []);
+
+
+angular.module('templates.common', []);
+
