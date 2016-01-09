df81b38b8822907693cb758e5976278d3233b6e9
ffandii <13593961606@163.com>
2016-01-09 15:42:52 +0800

    modified index.html and app.js of index.html

--- client/dist/scrum.js
+++ client/dist/scrum.js
@@ -1,4 +1,4 @@
-/* scrum - v 0.0.1 - 2016-01-08 
+/* scrum - v 0.0.1 - 2016-01-09 
 https://github.com/ffandii/Scrum 
  * Copyright (c) 2016 ffandii 
 */
@@ -6,10 +6,18 @@ https://github.com/ffandii/Scrum
  * Created by Administrator on 2016/1/3 0003.
  */
 
-angular.module('app',[]).constant('MONGOLAB_CONFIG',{  //connect ongodb config
+angular.module('app',[]);
+
+angular.module('app').constant('MONGOLAB_CONFIG',{  //connect ongodb config
     baseUrl: '/databases/',
     dbName: 'ffandii'
 });
+
+
+angular.module('app').controller('AppCtrl', function($scope){
+
+});
+
 angular.module('templates.app', []);
 
 
--- client/src/app/app.js
+++ client/src/app/app.js
@@ -2,7 +2,14 @@
  * Created by Administrator on 2016/1/3 0003.
  */
 
-angular.module('app',[]).constant('MONGOLAB_CONFIG',{  //connect ongodb config
+angular.module('app',[]);
+
+angular.module('app').constant('MONGOLAB_CONFIG',{  //connect ongodb config
     baseUrl: '/databases/',
     dbName: 'ffandii'
+});
+
+
+angular.module('app').controller('AppCtrl', function($scope){
+
 });
