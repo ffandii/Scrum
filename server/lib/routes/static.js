7f73dcbbff20d73dd86f880d49e9dc27da550928
ffandii <13593961606@163.com>
2016-01-09 10:13:26 +0800

    modified favicon to favicon.icon

--- server/lib/routes/static.js
+++ server/lib/routes/static.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 exports.addRoutes = function(app, config){
     //serve up the favicon
-    app.use(express.favicon(config.server.disFolder + '/favicon'));
+    app.use(express.favicon(config.server.disFolder + '/favicon.ico'));
 
     //first looks for a static file, index.html images, css, etc.
     app.use(config.server.staticUrl, express.compress());
