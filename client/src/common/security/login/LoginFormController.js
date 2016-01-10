2f4913c31ec6072513db90fc935df755d534864e
ffandii <13593961606@163.com>
2016-01-10 17:21:00 +0800

    add login form controller of common security

--- client/src/common/security/login/LoginFormController.js
+++ client/src/common/security/login/LoginFormController.js
@@ -1,3 +1,14 @@
-/**
- * Created by Administrator on 2016/1/10 0010.
- */
+angular.module('security.login.form', ['services.localizedMessages'])
+
+    .controller('LoginFormController', ['$scope', 'localizedMessages', function( $scope, localizedMessages ){
+
+        //the modal for the form
+        $scope.user = {};
+
+        //any error message from failing to login
+        $scope.authError = null;
+
+        //the reason that we are being asked to login, for instance , because we tried to access something to which we are not authorized now
+        $scope.authReason = localizedMessages.get('login.reason.notAuthorized');
+
+    }]);
