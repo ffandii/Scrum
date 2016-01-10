1aedc9de55461bc449219cc9f3d48c23010cce05
ffandii <13593961606@163.com>
2016-01-10 17:23:15 +0800

    add the main controller of the scrum project

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -2,11 +2,36 @@
  * Created by Administrator on 2016/1/3 0003.
  */
 
-angular.module('app',[]);
+angular.module('app',[
+
+    'ngRoute',
+
+    'templates.app',
+    'templates.common'
+
+]);
 
 angular.module('app').constant('MONGOLAB_CONFIG',{  //connect ongodb config
     baseUrl: '/databases/',
     dbName: 'ffandii'
+});
+
+//TODO remove those messages to a separate module
+angular.module('app').constant('I18N.MESSAGES',{
+
+    'errors.route.changeError' : '路由更改错误',
+    'crud.user.save.success' : "用户 '{{id}}' 被成功保存了。",
+    'crud.user.remove.success' : "用户 '{{id}}' 被成功删除了。",
+    'crud.user.save.error' : "保存用户 '{{id}}' 时出现了错误...",
+    'crud.user.remove.error' : "删除用户 '{{id}}' 时出现了错误...",
+    'crud.project.save.success' : "项目 '{{id}}' 被成功保存了。",
+    'crud.project.remove.success' : "项目 '{{id}}' 被成功删除了。",
+    'crud.project.save.error' : "保存项目 '{{id}}' 时出现了错误...",
+    'login.reason.notAuthorized' : "你没有必要的访问权限。你想作为其他人登录吗？",
+    'login.reason.notAuthenticated' : "你必须先登录才能访问这部分内容。",
+    'login.error.invalidCredentials' : "登录失败。请检查您的信息，然后重试。",
+    'login.error.serverError' : "服务端认证错误：'{{exception}}'。"
+
 });
 
 
