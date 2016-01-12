1ce1a546e331bc27ae3392ccf0e752585b46c3af
ffandii <13593961606@163.com>
2016-01-12 21:43:53 +0800

    add common service --- security

--- client/src/common/security/login/toolbar.js
+++ client/src/common/security/login/toolbar.js
@@ -1,3 +1,20 @@
-/**
- * Created by Administrator on 2016/1/12 0012.
- */
+angular.module('security.login.toolbar',[])
+
+//the login toolToolbar directive is a reusable widget that can show login or logout button
+//and information the current authenticated user
+
+    .directive('loginToolbar', function(){  //指令中提供的字段选项都是可选的
+
+        var directive = {
+            templateUrl : "security/login/toolbar.tpl.html",
+            restrict : "E",
+            replace : true,
+            scope : true,  //继承自己的父作用域还是创建一个独立的作用域
+            link : function( $scope, $element, $attrs, $controller ){
+
+            }
+        };
+
+        return directive;
+
+    });
--- client/src/common/security/retryQueue.js
+++ client/src/common/security/retryQueue.js
@@ -1,3 +1,77 @@
-/**
- * Created by Administrator on 2016/1/12 0012.
- */
+angular.module('security.retryQueue', [])
+
+//this is a generic retry queue for security failures. Each item is expected to expose to functions: retry and cancel
+.factory('securityRetryQueue', ['$q', '$log', function($q, $log){
+
+        var retryQueue = [];
+        var service = {
+            //the security service put its own handler in here
+            onItemAddedCallbacks : [],
+
+            hasMore : function(){
+                return retryQueue.length > 0;
+            },
+
+            push : function( retryItem ) {
+                retryQueue.push( retryItem );
+                //call all the onItemAdded callbacks
+                angular.forEach( service.onItemAddedCallbacks, function( cb ){
+                    try {
+                        cb( retryItem );
+                    } catch( e ){
+                        $log.error('securityRetryQueue.push(retryItem): callback threw an error: '+ e);
+                    }
+                });
+            },
+
+            pushRetryFn : function( reason, retryFn ){
+                //the reason parameter is optional
+                if( arguments.length === 1 ){
+                    retryFn = reason;
+                    reason = undefined;
+                }
+
+                //the deferred object that will be resolved or rejected by calling retry or cancel
+                var deferred = $q.defer();
+                var retryItem = {
+                    reason : reason,
+                    retry : function(){
+                        //wrap the result of the retryFn into a promise if it is not already
+                        $q.when(retryFn()).then(function(value){
+                            //if it was successful, then resolve our deferred
+                            deferred.resolve(value);
+                        }, function(value){
+                            //otherwise reject it
+                            deferred.reject(value);
+                        });
+                    },
+                    //give up our retrying and reject our deferred
+                    cancel : function(){
+                        deferred.reject();
+                    }
+                };
+                service.push(retryItem);
+                return deferred.promise;
+            },
+
+            retryReason : function(){
+                return service.hasMore() && retryQueue[0].reason;
+            },
+
+            cancelAll : function(){
+                while(service.hasMore()){
+                    retryQueue.shift().cancel();
+                }
+            },
+
+            retryAll : function(){
+                while(service.hasMore()){
+                    retryQueue.shift.retry();
+                }
+            }
+
+        };
+
+        return service;
+
+    }]);
