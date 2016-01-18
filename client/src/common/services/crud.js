9a96910f37cf8b573b8f2a8f4c292a89eea5f5dd
ffandii <13593961606@163.com>
2016-01-18 20:08:42 +0800

    add crud route and curd services

--- client/src/common/services/crud.js
+++ client/src/common/services/crud.js
@@ -1,3 +1,72 @@
-/**
- * Created by Administrator on 2016/1/18 0018.
- */
+angular.module('services.crud', ['services.crudRouteProvider']);
+
+angular.module('services.crud').factory('crudEditMethods',function(){
+
+    return function(itemName, item, formName, successcb, errorcb){
+
+        var mixin = {};
+
+        mixin[itemName] = item;
+        mixin[itemName+'Copy'] = angular.copy(item);
+
+        mixin.save=  function(){
+            this[itemName].$saveOrUpdate(successcb, successcb, errorcb, errorcb);
+        };
+
+        mixin.canSave = function(){
+            return this[formName].$valid && !angular.equals(this[itemName], this[itemName+'Copy']);
+        };
+
+        mixin.revertChanges = function(){
+            this[itemName] = angular.copy(this[itemName+'Copy']);
+        };
+
+        mixin.canRevert = function(){
+            return !angular.equals(this[itemName],this[itemName+'Copy']);
+        };
+
+        mixin.remove = function(){
+            if(this[itemName].$id()){
+                this[itemName].$remove(successcb, errorcb);
+            } else {
+                successcb();
+            }
+        };
+
+        mixin.canRemove = function(){
+            return item.$id();
+        };
+
+        mixin.getCssClasses = function(fieldName){
+            var ngModelController = this[formName][fieldName];
+            return {
+                error : ngModelController.$invalid && ngModelController.$dirty,
+                success : ngModelController.$valid && ngModelController.$dirty
+            };
+        };
+
+        return mixin;
+
+    };
+
+});
+
+angular.module('services.crud').factory('crudListMethods',['$location',function($location){
+
+    return function(pathPrefix){
+
+        var mixin = {};
+
+        mixin['new'] = function(){
+            $location.path(pathPrefix + '/new');
+        };
+
+        mixin['edit'] = function(itemId){
+            $location.path(pathPrefix +'/'+itemId);
+        };
+
+        return mixin;
+
+    }
+
+}]);
--- client/src/common/services/crudRouteProvider.js
+++ client/src/common/services/crudRouteProvider.js
@@ -1,3 +1,92 @@
-/**
- * Created by Administrator on 2016/1/18 0018.
- */
+(function(){
+
+    function crudRouteProvider($routeProvider){
+
+        //this $get noop is because at the moment in angularjs "providers" must provide something
+        //via a $get method
+        //when angularjs has "provide helpers" then this will go away
+        this.$get = angular.noop;
+
+        //in any case the point is that this function is the key part of this "provider helper"
+        //we use it to create routes for CRUD operations.we give it some basic information about the resource and the urls
+        // then it returns our own special routesProvider
+        this.routesFor = function( resourceName, urlPrefix, routePrefix ){
+
+            var baseUrl = resourceName.toLowerCase();
+            var baseRoute = '/' + resourceName.toLowerCase();
+            routePrefix = routePrefix || urlPrefix;
+
+            //prepend the urlPrefix if it is available
+            if( angular.isString(urlPrefix) && urlPrefix !== "" ){
+                baseUrl = urlPrefix + "/" + baseUrl;
+            }
+
+            //prepend the routePrefix if it is provided
+            if( routePrefix !== null &&  routePrefix !== undefined && routePrefix !== ""){
+                baseRoute = '/' + routePrefix + baseRoute;
+            }
+
+            //create the template url for a route to our resource that does the specified operation
+            var templateUrl = function(operation){
+                return baseUrl + '/' + resourceName.toLowerCase() + '-' + operation + '.tpl.html';
+            };
+
+            //create the controller name for a route to our resource that does the specified operation
+            var controllerName = function(operation){
+                return resourceName + operation + 'Ctrl';
+            };
+
+            //this is the object that our 'RouteFor() function returns. it decorate $routeProvider
+            //delegate the when() and otherwise() functions but also exposing some new functions for
+            //creating new crud routes
+
+            var routeBuilder = {
+
+                //create a route that will showing a list of items
+                whenList : function(resolveFns){
+                    routeBuilder.when(baseRoute,{
+                        templateUrl : templateUrl('List'),
+                        controller : controllerName('List'),
+                        resolve : resolveFns
+                    });
+                    return routeBuilder;
+                },
+                //creating a route that will handle creating a new item
+                whenNew : function(resolveFns){
+                    routeBuilder.when(baseRoute+'/new',{
+                        templateUrl : templateUrl('Edit'),
+                        controller : controllerName('Edit'),
+                        resolve : resolveFns
+                    });
+                    return routeBuilder;
+                },
+                //creating a route that will handle editing an existing item
+                whenEdit : function(resolveFns){
+                    routeBuilder.when(baseRoute +'/:itemId',{
+                        template : templateUrl('Edit'),
+                        controller : controllerName('Edit'),
+                        resolve : resolveFns
+                    });
+                    return routeBuilder;
+                },
+                when : function(path,route){
+                    $routeProvider.when(path,route);
+                    return routeBuilder;
+                },
+                otherwise : function(params){
+                    $routeProvider.otherwise(params);
+                    return routeBuilder;
+                },
+                $routeProvider : $routeProvider
+            };
+
+        };
+
+    }
+
+    crudRouteProvider.$injector = ['$routeProvider'];
+
+    angular.module('services.crudRouteProvider',['ngRoute']).provider('crudRoute',crudRouteProvider);
+
+
+})();
